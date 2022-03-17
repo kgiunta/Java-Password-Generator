48e38d1086b368ccbdbccadc52db0f4e33e2e96c
Kgiunta <giunta.kyle@yahoo.com>
2022-03-16 22:45:09 -0400

    Finalized with solutions

--- script.js
+++ script.js
@@ -1,72 +1,96 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// var aNumber =[ Range(8,120) ];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "+", "?"];
+var numbers = ["1","2","3","4","5","6","7","8","9","0"];
+var totalChoices = [];
 
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-
-// window.alert("You must complete the following prompts for a new password")
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-
-
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-
-
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-function generatePassword(){
-  var aNumber = Number(window.prompt("Please choose at least 8 characters and no more than 128 characters", ""));
-  console.log(aNumber)
-  if(isNaN(aNumber)){
-    window.alert("This is not a number, try again.")
-    return "Please try again"
+function generatePassword() {
+  var aNumber = Number(
+    window.prompt(
+      "Please choose a password length of at least 8 characters and no more than 128 characters",
+      ""
+    )
+  );
+  console.log(aNumber);
+  if (isNaN(aNumber)) {
+    window.alert("This is not a number, try again.");
+    return "Please try again";
   }
-  if( aNumber<8){
-    window.alert("The number is below 8 characters")
-    return "Please try again"
+  if (aNumber < 8) {
+    window.alert("The number is below 8 characters");
+    return "Please try again";
   }
-  if(aNumber>128){
-    window.alert("This number is above 128 characters")
-  return "Please try again"
+  if (aNumber > 128) {
+    window.alert("This number is above 128 characters");
+    return "Please try again";
   }
-var lowerChoice = window.confirm("Do you want lowercase in your password")
-console.log(lowerChoice)
+  // WHEN asked for character types to include in the password
+  // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
 
+  var password = "";
+  var lowerChoice = window.confirm("Do you want lowercase in your password");
+  console.log(lowerChoice);
+  // get one lowercase letter
+  // add all lower case letters to the possible combos
+  if (lowerChoice == true) {
+    var lowerIndex = Math.floor(Math.random() * lowerCase.length);
+    var lowerLetter = lowerCase[lowerIndex];
+    password += lowerLetter;
+    totalChoices = totalChoices.concat(lowerCase);
+  }
 
-// if (lowerChoice=false) {
-//   [ABCDEFGHIJKLMNOPQRSTUVWXYZ]
-// }else {[abcdefghijklmnopqrstuvwxyz]
+  var upperChoice = window.confirm("Do you want uppercase in your password");
+  console.log(upperChoice);
+  if (upperChoice == true) {
+    var upperIndex = Math.floor(Math.random() * upperCase.length);
+    var upperLetter = upperCase[upperIndex];
+    password += upperLetter;
+    totalChoices = totalChoices.concat(upperCase);
+  }
 
-// }
-
-// var specialChar
-// if(specialChar = true){
- 
-// ["!", "@", "#", "$", "%", "^", "&", "*", "+", "?"]
-// }else{
-
-// }
-
-
-
-
+  var specialCharChoice = window.confirm(
+    "Do you want special characters in your password"
+  );
+  console.log(specialCharChoice);
+  if (specialCharChoice == true) {
+    var charIndex = Math.floor(Math.random() * specialChar.length);
+    console.log(specialChar.length);
+    var charLetter = specialChar[charIndex];
+    password += charLetter;
+    console.log(charIndex);
+    totalChoices = totalChoices.concat(specialChar);
+  }
+  var num = window.confirm("Do you want numbers in your password");
+  console.log(num);
+  if (num == true) {
+    var numbIndex = Math.floor(Math.random() * numbers.length);
+    var numLetter = numbers[numbIndex];
+    password += numLetter;
+    totalChoices = totalChoices.concat(numbers);
+  }
+  if (!num && !specialCharChoice && !upperChoice && !lowerChoice) {
+    window.alert("PLEASE SELECT AT LEAST ONE CRITERIA");
+    return "";
+  }
+  for (let index = password.length; index < aNumber; index++) {
+    var indexPull = Math.floor(Math.random() * totalChoices.length);
+    var generator = totalChoices[indexPull];
+    password += generator;
+  }
+  // scrambles password in true random fashion
+  password = [...password].sort(() => Math.random() - 0.5).join("");
+  totalChoices = [];
+  return password;
 }
-  // Write password to the #password input
+
+// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
